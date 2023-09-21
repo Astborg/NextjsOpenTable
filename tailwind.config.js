ca7854bf1e963afd3edc72a94674f7334b029790
Astborg <93790636+Astborg@users.noreply.github.com>
2023-09-21 11:50:20 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      "2xsm": "10px",
+      "xsm":"12px",
+      "sm": "13px",
+      "reg": "15px",
+      "lg": "18px",
+      "2xl": "22px",
+      "2xl": "25px",
+      "2xl": "32px",
+      "2xl": "40px",
+      "2xl": "50px",
+      "2xl": "70px",
+    }
+  },
+  plugins: [],
+}
